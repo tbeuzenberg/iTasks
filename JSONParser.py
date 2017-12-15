56346b1f84c35cc3445d61572ea42b2fd4fdc769
Mika Edelenbos <mikaedelenbos@gmail.com>
2017-12-15 15:18:36 +0100

    Both file and string support
    Signed-off-by: Mika Edelenbos <mikaedelenbos@gmail.com>

--- JSONParser.py
+++ JSONParser.py
@@ -1,5 +1,9 @@
 import json
 
 
-def json_decode(strIn):
-    return json.loads(strIn)
+def json_decode_string(str_in):
+    return json.loads(str_in)
+
+
+def json_decode_file(file_in):
+    return json.load(file_in)
--- TestEnviroment.py
+++ TestEnviroment.py
@@ -1,16 +1,14 @@
-import json
 import unittest
-
-import JSONParser
 
 
 # Example Unittest
+""""
 class TestJSONParser(unittest.TestCase):
 
     def test_decoder(self):
-        result = JSONParser.json_decode('["foo", {"bar":["baz", null, 1.0, 2]}]')
+        result = JSONParser.json_decode_string('["foo", {"bar":["baz", null, 1.0, 2]}]')
         self.assertEqual(result, json.loads('["foo", {"bar":["baz", null, 1.0, 2]}]'))
-
+"""""
 
 if __name__ == '__main__':
     unittest.main()
