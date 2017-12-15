95cc780bb4e8e613b6f90cd3568aee1cb9c30587
nick <hiddingnick@gmail.com>
2017-12-15 13:48:25 +0100

    #w5ek - Made iTasksService compatible with Windows

--- iTasksService.py
+++ iTasksService.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 
-import fcntl
 import os
 import json
 from subprocess import *
@@ -22,9 +21,16 @@ class ItasksService:
 
     def start_server(self):
         # Start iTasks server
-        process = Popen(
-            ["/usr/bin/mono", "itasks_server/iTasksToStdIO.exe"],
-            stdout=PIPE, stdin=PIPE, bufsize=0)
+        global process
+        if os.name is "nt":
+            process = Popen(
+                ["itasks_server/iTasksToStdIO.exe"],
+                stdout=PIPE, stdin=PIPE, bufsize=0)
+        elif os.name is "posix":
+            process = Popen(
+                ["/usr/bin/mono", "itasks_server/iTasksToStdIO.exe"],
+                stdout=PIPE, stdin=PIPE, bufsize=0)
+
         thread = Thread(target=self.background_worker, args=[process.stdout])
         thread.daemon = True
         thread.start()
@@ -42,9 +48,9 @@ class ItasksService:
 
     @staticmethod
     def non_block_read(output):
-        fd = output.fileno()
-        fl = fcntl.fcntl(fd, fcntl.F_GETFL)
-        fcntl.fcntl(fd, fcntl.F_SETFL, fl | os.O_NONBLOCK)
+        # fd = output.fileno()
+        # fl = fcntl.fcntl(fd, fcntl.F_GETFL)
+        # fcntl.fcntl(fd, fcntl.F_SETFL, fl | os.O_NONBLOCK)
         try:
             line = output.readline().decode('utf-8')
             if len(line) > 1:
--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 import sys
+import os
 from PyQt5.QtWidgets import QMainWindow, QWidget, QPushButton, QHBoxLayout, QVBoxLayout, QApplication
 from iTasksService import ItasksService
 import json
@@ -71,7 +72,11 @@ class Example(QMainWindow):
             self.itasksService.send_data('["event",1,["1-61",null,"Start task"]]')
         if self.temp_start_palindrome is 4:
             object = json.loads(data)
-            attributes = object['change']['children'][0][2]['children'][0][2]['definition']['children'][1]['children'][0]['children'][0]['attributes']
+            global attributes
+            if os.name is "nt":
+                attributes = object['change']['children'][0][2]['children'][0][2]['children'][0][2]['children'][1][2]['children'][0]['attributes']
+            elif os.name is "posix":
+                attributes = object['change']['children'][0][2]['children'][0][2]['definition']['children'][1]['children'][0]['children'][0]['attributes']
             instance_no = attributes['instanceNo']
             instance_key = attributes['instanceKey']
             self.itasksService.attach_task_instance(instance_no, instance_key, self.task_callback)
