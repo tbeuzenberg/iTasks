ffd3214c3d15e08cb91adfb1403fa73ac99340db
Thijs van Dam <thijsvandam@msn.com>
2017-12-22 11:57:29 +0100

    #wbr8 - Change JSONParser to TreeComponents

--- test/test_tree.py
+++ test/test_tree.py
@@ -11,7 +11,7 @@ from TreeComponents import (
 
 class TreeTest(unittest.TestCase):
 
-    @patch('JSONParser.Node.search_node')
+    @patch('TreeComponents.Node.search_node')
     def test_search_node(self, search_node_mock):
         """
 
@@ -28,7 +28,7 @@ class TreeTest(unittest.TestCase):
         # Assert
         search_node_mock.assert_called_with(index_list=[0])
 
-    @patch('JSONParser.Node.add_or_replace_child')
+    @patch('TreeComponents.Node.add_or_replace_child')
     def test_insert_with_single_digit_location(self, add_or_replace_child_mock):
         # Assign
         root = Node()
@@ -36,14 +36,14 @@ class TreeTest(unittest.TestCase):
         to_be_added = Node()
 
         # Act
-        with patch('JSONParser.Node.__new__', return_value={"AA": "AAA"}):
+        with patch('TreeComponents.Node.__new__', return_value={"AA": "AAA"}):
             tree.insert(value=to_be_added, index_list=[3])
 
         # Assert
         add_or_replace_child_mock.assert_called_with(node={"AA": "AAA"}, index=3)
 
-    @patch('JSONParser.Node.search_node')
-    @patch('JSONParser.Node.add_or_replace_child')
+    @patch('TreeComponents.Node.search_node')
+    @patch('TreeComponents.Node.add_or_replace_child')
     def test_insert_with_double_digit_location(self, add_or_replace_child_mock, search_node_mock):
         # Assign
         root = Node()
@@ -53,7 +53,7 @@ class TreeTest(unittest.TestCase):
         search_node_mock.return_value = child
 
         # Act
-        with patch('JSONParser.Node.__new__', return_value={"AA": "AAA"}):
+        with patch('TreeComponents.Node.__new__', return_value={"AA": "AAA"}):
             tree.insert(value="Node name", index_list=[3, 0])
 
         # Assert
