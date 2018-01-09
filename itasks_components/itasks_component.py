b8b0ba378ffcbe947ce93da8e2aa6fd5f2476a81
Michiel-PC\Michiel <michiel-daams@hotmail.com>
2018-01-09 10:45:26 +0100

    #wu3m added task_id to ItasksComponent

--- itasks_components/itasks_component.py
+++ itasks_components/itasks_component.py
@@ -7,7 +7,8 @@ from ui_generator import components
 
 class ItasksComponent:
 
-    def __init__(self, qwidget=None, action_id=None, qlayout: QLayout=None):
+    def __init__(self, qwidget=None, action_id=None, task_id=None, qlayout: QLayout=None):
         self.action_id = action_id
+        self.task_id = task_id
         self.qwidget = qwidget
         self.qlayout = qlayout
